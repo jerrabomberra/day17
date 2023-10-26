52f6a475e82cd844cd999348fad38ca74b742674
Doug Gordon <gordononline@bigpond.com>
2023-10-26 20:58:12 +1100

    adding to classes

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ class User:
         """initialises attributes"""
         self.user_id=user_id
         self.user_name=user_name
-
+        self.followers = 3
 
     def printit(self, user_id, user_name):
         """prints something out"""
-        print(f"Your user id is {user_id}, and your user name is {user_name}")
+        print(f"Your user id is {user_id}, your user name is {user_name} \
+and you have {self.followers} followers.")
 
 
 user_1=User("001","Doug")
+user_1.followers +=6
 user_1.printit('001','Doug')
+# print(user_1.followers)
 
-
-user_2=User("002","Bob")
-user_2.printit('002','Bob')
+# user_2=User("002","Bob")
+# user_2.printit('002','Bob')
